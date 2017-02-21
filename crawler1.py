ce7b5d376b45b6d63f84050e857784db1d2c921f
Warner <rampy@vt.edu>
2017-02-20 23:16:47 -0500

    initial commit - documentation example

---
+++ crawler1.py
@@ -0,0 +1,22 @@
+# initial commit using example from documentation
+# run "scrapy runspider crawler1.py -o quotes.json in" terminal
+
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.xpath('span/small/text()').extract_first(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
